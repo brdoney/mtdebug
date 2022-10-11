0c4936638baff5c47f966e311f33e707da5c365c
Brendan Doney <bre.doney@gmail.com>
2022-10-11 13:53:23 -0400

    Create plan for server code

---
+++ demoserv.py
@@ -0,0 +1,12 @@
+from flask import Flask
+# takes in input of file to run
+# starts gdbmi instance, then 
+
+app = Flask(__name__)
+
+# Start up pygdmi
+
+# use POST for commands - take an argument (command), run it, then give all
+# output back to host
+
+# @app.route("/")
---
+++ libc-intercept.c
@@ -0,0 +1,2 @@
+// Interface with a socket that the server will open
+// Intercept libc sync API calls to track things, updates will be given over the socket
