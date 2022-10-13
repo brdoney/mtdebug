b09b6bd9891a966cb14bd07afabd58a6eaff079c
joshua88 <joshua88@vt.edu>
2022-10-12 23:51:04 -0400

    basic backend demo of server

--- demoserv.py
+++ demoserv.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from pygdbmi.gdbcontroller import GdbController
 # takes in input of file to run
 # starts gdbmi instance, then 
 
 app = Flask(__name__)
 
 # Start up pygdmi
+gdbmi = GdbController()
 
 # use POST for commands - take an argument (command), run it, then give all
 # output back to host
+@app.route("/")
+def input():
+    return render_template('input-command.html')
+
+@app.route("/output", methods=['POST'])
+def output():
+    input = request.form['command']
+    output = "invalid command"
+    if input == "start":
+        output = to_stdout(gdbmi.write('-file-exec-and-symbols a.out'))
+    elif input == "run":
+        output = to_stdout(gdbmi.write('-exec-run'))
+    return output
+
+# look for messages to stdout
+def to_stdout(response):
+    output = ''
+    for json in response:
+        if json['stream'] == 'stdout' and json['message'] != None:
+            output = output + json['message'] + '<br>'
+    return output
 
 # @app.route("/")
+app.run()
