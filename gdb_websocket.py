fbf22606d73933e9a0b47d139490e9750e9307b1
Brendan Doney <bre.doney@gmail.com>
2022-11-08 03:14:24 -0500

    Use pipes to talk to MI component in GDBController

--- gdb_websocket.py
+++ gdb_websocket.py
@@ -2,6 +2,7 @@ import io
 import os
 import pty
 import signal
+import subprocess
 from typing import cast
 
 from pygdbmi.gdbcontroller import DEFAULT_GDB_LAUNCH_COMMAND
@@ -11,8 +12,7 @@ from pygdbmi.IoManager import IoManager
 
 class GdbController:
     gdb: IoManager
-    gdb_pid: int
-    using_mi: bool
+    gdb_process: subprocess.Popen
 
     # stdin/out that the gdb instance is using (really just a pseudoterminal)
     # Writing and reading may seem flipped, but remember we are observing the pty
@@ -38,51 +38,37 @@ class GdbController:
         startup_commands_str = [f"-iex={c}" for c in startup_commands]
         command = [*DEFAULT_GDB_LAUNCH_COMMAND, *startup_commands_str]
         command = DEFAULT_GDB_LAUNCH_COMMAND
-        print(command)
 
-        pid, termfd = pty.fork()
-
-        if pid == 0:
-            # Child: start gdb
-            os.execvp(command[0], command)
-
-        # Parent: record the info
-        self.gdb_pid = pid
+        self.gdb_process = subprocess.Popen(
+            command,
+            shell=False,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            bufsize=0,
+        )
 
         # Create in/out off of pty and IoManager to interact w/ gdb through MI
         self.gdb = IoManager(
-            os.fdopen(termfd, mode="wb"),
-            os.fdopen(termfd, mode="rb"),
-            None,
+            self.gdb_process.stdin,  # type: ignore
+            self.gdb_process.stdout,  # type: ignore
+            self.gdb_process.stderr,  # type: ignore
         )
 
         self.using_mi = True
 
-    def terminate(self):
-        """
-        Kill the GDB process we're controlling and ensure that future calls to read and
-        write will fail.
-        """
-        if self.gdb is not None:
-            os.kill(self.gdb_pid, signal.SIGKILL)
-            self.gdb = None  # type: ignore
-            self.gdb_pid = -1
-            self.mi_stdin = None  # type: ignore
-            self.mi_stdout = None  # type: ignore
-
-    def set_mi(self, using_mi: bool) -> None:
-        """Set whether we are using GDB's MI to influence what read and write do."""
-        self.using_mi = using_mi
-
     def read(
-        self, timeout_sec: float = DEFAULT_GDB_TIMEOUT_SEC, raise_error_on_timeout=True
+        self,
+        timeout_sec: float = DEFAULT_GDB_TIMEOUT_SEC,
+        raise_error_on_timeout=True,
+        using_mi: bool = True,
     ):
         """
         If we are using MI right now, get and parse GDB response. Otherwise, read
         bytes directly from the pty connected to the gdb instance. See
         `IoManager.get_gdb_response()` for details about flags
         """
-        if self.using_mi:
+        if using_mi:
             return self.gdb.get_gdb_response(timeout_sec, raise_error_on_timeout)
         return self.tui_stdout.read()
 
@@ -92,13 +78,14 @@ class GdbController:
         timeout_sec=DEFAULT_GDB_TIMEOUT_SEC,
         raise_error_on_timeout: bool = True,
         read_response: bool = True,
+        using_mi: bool = True,
     ):
         """
         If we are using MI right now, write using pygdmi. Otherwise, write
         bytes directly to the pty connected to the gdb instance. See
         `IoManager.write()` for details about flags
         """
-        if self.using_mi:
+        if using_mi:
             mi_cmd_to_write = cast(str | list[str], data)
             print(mi_cmd_to_write)
             return self.gdb.write(
