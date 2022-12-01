6d3c5b114678800d17288a7d190badbb8ba87b19
Brendan Doney <bre.doney@gmail.com>
2022-12-01 09:05:12 -0500

    Move start button and breakpoints out of `ThreadWindow`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
 import "./App.css";
 import GDBTerminal from "./Terminal";
 import ThreadWindow from "./ThreadWindow";
+import BreakpointForm from "./BreakpointForm";
+import StartButton from "./StartButton";
 
 function App() {
   return (
     <div className="App">
       <GDBTerminal class="terminal" />
-      <div className="windows">
-        <ThreadWindow />
-        <ThreadWindow />
+
+      <div className="controls">
+        <BreakpointForm />
+        <div className="start-button">
+          <StartButton />
+        </div>
+
+        <div className="windows">
+          <ThreadWindow />
+          <ThreadWindow />
+        </div>
       </div>
     </div>
   );
--- frontend/src/ThreadWindow.jsx
+++ frontend/src/ThreadWindow.jsx
@@ -1,27 +1,17 @@
+import "./ThreadWindow.css";
+
 import ThreadPicker from "./ThreadPicker";
 import Step from "./Step";
-import BreakpointForm from "./BreakpointForm";
-import StartButton from "./StartButton";
 import { useState } from "react";
 
 export default function ThreadWindow() {
   const [selectedThreadId, setSelectedThreadId] = useState(null);
 
   return (
-    <div>
-      <div className="command-form">
-        <ThreadPicker
-          thread={selectedThreadId}
-          setThread={setSelectedThreadId}
-        />
+    <div className="command-form">
+      <ThreadPicker thread={selectedThreadId} setThread={setSelectedThreadId} />
 
-        <Step thread={selectedThreadId} />
-
-        <div>
-          <BreakpointForm />
-          <StartButton />
-        </div>
-      </div>
+      <Step thread={selectedThreadId} />
     </div>
   );
 }
